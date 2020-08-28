08d5d9a793a13d65ec49fbf0ffd7cdd05b5ae167
nexxtin.ashish <nexxtin.ashish@gmail.com>
2020-08-27 18:40:13 -0700

    tut2

---
+++ js/tut2.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#div1").click(function() {
+        $("#div2").slideToggle();
+    });
+    $("#div3").click(function() {
+        $("#div4").slideToggle();
+
+    });
+    $("#div5").click(function() {
+        $("div6").slideToggle();
+    });
+
+    $('#scrollToTop a').click(function(e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+});
